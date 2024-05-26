1698111de5ec85dad2789fee965e78d16e7709a7
Neil <neilwrobinson@users.noreply.github.com>
2024-05-26 13:31:36 -0400

    add: sidekiq configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require "sidekiq/web" # Brings in the Sidekiq Web UI
+
 Rails.application.routes.draw do
   resources :photoscws, path: :exposure # using carrier wave
   resources :tags
@@ -10,5 +12,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root to: "home#index"
+
+
+  # make the Sidekiq web UI available on /sidekiq
+  # TODO: update before launch into production 
+  mount Sidekiq:: Web => "/sidekiq"
 
 end
